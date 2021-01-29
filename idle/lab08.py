accdd507acc62c40add2b79655ac68f5a9e2948f
anthonycsilva <anthonycsilva@github.com>
2021-01-29 14:22:03 -0300

    lab08 feito

--- idle/lab08.py
+++ idle/lab08.py
@@ -15,4 +15,16 @@ def erro():
         numero += 1
     return (numero, soma(numero))
 
-print(erro())
+
+''' Numero 2 '''
+
+
+def busca_contato2(lista,nome):
+    '''essa função recebe uma lista e pesquisa por um determinado nome de pessoa, e retorna uma lista com as informações encontradas'''
+    '''list, str -> list'''
+    nomes_encontrados = list()
+
+    for i in range(len(lista)):
+        if (nome in lista[i][0]):
+            nomes_encontrados.append(lista[i])
+    return nomes_encontrados
